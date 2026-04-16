efc2f2ef742c353478cf5c5bffba588c1373f25d
Sepehr Rajabi <sepehrrajabi478@gmail.com>
2026-04-17 01:36:34 +0200

    Added ability to pipe the output directly to `docker run --env-file`

---
+++ cmd/docker.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/SepehrRajabi/envvault/crypto"
+	"github.com/SepehrRajabi/envvault/envfile"
+	"github.com/spf13/cobra"
+)
+
+var dockerOutput string
+
+var dockerCmd = &cobra.Command{
+	Use:   "docker [vault-file]",
+	Short: "Output decrypted secrets in Docker --env-file format",
+	Long: "Decrypts a .env.vault file and outputs KEY=VALUE lines compatible with 'docker run --env-file'.\n" +
+		"Supports ENVVAULT_PASSWORD for non-interactive use.",
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// 1. Read the vault file
+		filePath := args[0]
+
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			return fmt.Errorf("reading %s: %w", filePath, err)
+		}
+
+		// 2. Get password (Env var or prompt)
+		password, err := crypto.GetPassword("Enter your Password: ")
+		if err != nil {
+			return err
+		}
+
+		// 3. Decrypt
+		var p crypto.Provider
+		if algorithm != "" {
+			var err error
+			p, err = crypto.GetProvider(algorithm)
+			if err != nil {
+				return fmt.Errorf("unknown algorithm %q: %w", algorithm, err)
+			}
+		}
+		decrypted, err := crypto.Decrypt(data, password, p)
+		if err != nil {
+			return fmt.Errorf("decryption failed: %w", err)
+		}
+
+		// 4. Parse .env contents
+		vars, err := envfile.Parse(string(decrypted))
+		if err != nil {
+			return fmt.Errorf("parsing env file: %w", err)
+		}
+
+		// 5. Determine output destination
+		var out *os.File
+		if dockerOutput != "" {
+			out, err = os.Create(dockerOutput)
+			if err != nil {
+				return fmt.Errorf("creating output file: %w", err)
+			}
+			defer out.Close()
+			// Restrict permissions since it contains raw secrets temporarily
+			out.Chmod(0600)
+		} else {
+			out = os.Stdout
+		}
+
+		// 6. Write clean KEY=VALUE format
+		for _, v := range vars {
+			fmt.Fprintf(out, "%s=%s\n", v.Key, v.Value)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	dockerCmd.Flags().StringVarP(&dockerOutput, "output", "o", "", "Write to a file instead of stdout")
+	rootCmd.AddCommand(dockerCmd)
+}
