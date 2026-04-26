6579d58716d9a2f501d568d05ebfed42669c1ef1
Sepehr Rajabi <sepehrrajabi478@gmail.com>
2026-04-27 00:09:00 +0200

    Added `ENVVAULT_DEFAULT_PROVIDER` for getting default encryption algorithm

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/SepehrRajabi/envvault/cmd"
 	"github.com/SepehrRajabi/envvault/crypto"
 )
 
 func main() {
-	crypto.SetDefault("aes256gcm-argon2id")
+	defaultProvider := os.Getenv("ENVVAULT_DEFAULT_PROVIDER")
+	if defaultProvider == "" {
+		defaultProvider = "aes256gcm-argon2id"
+	}
+	crypto.SetDefault(defaultProvider)
+
 	cmd.Execute()
 }
