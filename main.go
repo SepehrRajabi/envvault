ff0b52d480a1daaf767c66772fbd839b4b87647f
Sepehr Rajabi <sepehrrajabi478@gmail.com>
2026-05-05 22:50:46 +0200

    Added `IsBeingTraced` to make the program detect if a debuger / tracer is attached

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/SepehrRajabi/envvault/cmd"
 	"github.com/SepehrRajabi/envvault/crypto"
 )
 
 func main() {
+	debugMode := strings.ToLower(os.Getenv("DEBUG"))
+	if debugMode == "0" || debugMode == "false" {
+		if isDebugging, _ := crypto.IsBeingTraced(); isDebugging {
+			fmt.Println("Security Error: Tracer detected. Exiting for safety.")
+			os.Exit(1)
+		}
+	}
+
 	defaultProvider := os.Getenv("ENVVAULT_DEFAULT_PROVIDER")
 	if defaultProvider == "" {
 		defaultProvider = "aes256gcm-argon2id"
